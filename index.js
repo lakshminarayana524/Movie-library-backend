84a64a512502ba40f41577b74801ed765f29b86f
Lakshmi Narayana <lakshminarayana52004@gmail.com>
2024-06-02 22:23:52 +0530

    changed the url deploy

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://movie-library-gln.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"], // Change "UPDATE" to "PUT"
     credentials: true,
 }));
